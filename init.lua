c3b08cb35b708d76c6975400b7234c6abb1266e4
ProfXwing <50530928+ProfXwing@users.noreply.github.com>
2024-03-24 20:39:22 -0400

    update

--- init.lua
+++ init.lua
@@ -92,6 +92,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Display long lines as just one line
+vim.opt.wrap = false
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
